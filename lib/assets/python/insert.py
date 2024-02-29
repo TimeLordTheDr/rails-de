8e672a711ffd98a6ff8d29e3c3cb965b07daffd2
Ubuntu <ubuntu@ip-172-31-42-29.ec2.internal>
2024-02-29 21:37:48 +0000

    pip uninstall mysql-connector->i mysql-connector-python(w3s recommended

--- lib/assets/python/insert.py
+++ lib/assets/python/insert.py
@@ -12,7 +12,7 @@ connection_mysql = mysql.connector.connect(
 cursor = connection_mysql.cursor()
 
 # Include values for 'created_at' and 'updated_at'
-cursor.execute('INSERT INTO notes (title, content, level, datetime, created_at, updated_at) VALUES ("title", "content", 43, %s, %s, %s)',
+cursor.execute('INSERT INTO notes (title, content, level, datetime, created_at, updated_at) VALUES ("title", "content", 173, %s, %s, %s)',
                (datetime.now(), datetime.now(), datetime.now()))
 
 connection_mysql.commit()
